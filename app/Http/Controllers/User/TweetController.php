8fc908c5a40b9ea59b41cff7bcbf4c161bd072e7
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-11-01 16:48:58 +0300

    code cleanup

--- app/Http/Controllers/User/TweetController.php
+++ app/Http/Controllers/User/TweetController.php
@@ -35,12 +35,6 @@ class TweetController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
